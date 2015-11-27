a85ac0e1628878ca6ff6b1da9710fc45f2237574
Benjamin Carrera <benjamin_carrera@live.com.mx>
2015-11-27 10:57:07 -0600

    inicio de integracion de buro

---
+++ application/modules/tempciri/views/autorizacion-buro-view.php
@@ -0,0 +1,92 @@
+<form>
+	<div class="pdf-container" style="font-size:9px;">
+		<h4 class="text-center" style="color:#FFFFFF; background-color: rgb(175, 167, 167); padding: 6px 0; font-weight: bold;">Pasaje de Electronica El Salvador  S.A de C.V.</h4>
+		<br/>
+		<div class="row">
+			<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<h6>Plaza</h6>
+		 	</div>
+		  	<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<h6>Folio de Autorizacion</h6>
+		  	</div>
+		</div>
+		<h4 class="text-center"><strong>Autorizacion Buro de Credito Persona Moral</strong></h4>
+		<br/>
+		<div class="row">
+			<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<h6>Lugar de Autorizacion</h6>
+		 	</div>
+		  	<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<h6>Fecha de Autorizacion</h6>
+		  	</div>
+		</div>
+		<br/>
+		<p class="text-center" style="font-size:11px;">Por este conducto autorizo expresamente a Plaza Lopez Cotilla S.A de C.V. para que por conducto de sus funcionarios facultados lleve a cabo Investigaciones, sobre mi comportamiento crediticio o el de la Empresa que represento en Trans Union de México, S. A. SIC  y/o Dun & Bradstreet, S.A. SIC. Así mismo, declaro que conozco la naturaleza y alcance de la información que se solicitará, del uso que  Plaza Lopez Cotilla S.A de C.V hará de tal información y de que ésta podrá realizar consultas periódicas sobre mi historial o el de la empresa que represento, consintiendo que esta autorización se encuentre vigente por un período de 3 años contados a partir de su expedición y en todo caso durante el tiempo que se mantenga la relación jurídica.</p>
+		<br/>
+		<p class="text-center" style="font-size:11px;">En caso de que la solicitante sea una Persona Moral,  declaro bajo protesta de decir verdad Ser Representante Legal de la empresa mencionada en esta autorización; manifestando que a la fecha de firma de la presente autorización los poderes no me han sido revocados, limitados, ni modificados en forma alguna.</p>
+		<br/>
+		<div class="row">
+			<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<p>Persona Moral (PM)</p>
+		 	</div>
+		  	<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<p>RFC</p>
+		  	</div>
+		</div>
+		<br/>
+		<p>Nombre de Representante Legal</p>
+		<br/>
+		<p>Razon Social de la Empresa</p>
+		<br/>
+		<p>Calle y Numero</p>
+		<br/>
+		<p>Colonia o Poblacion</p>
+		<br/>
+		<p>Delegacion o Municipio</p>
+		<br/>
+		<div class="row">
+			<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<p>Ciudad</p>
+		  		<br/>
+		  		<p>Codigo Postal</p>
+		 	</div>
+		  	<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<p>Estado</p>
+		  		<br/>
+		  		<p>Telefono</p>
+		  	</div>
+		</div>
+		<br/>
+		<p class="text-center" style="font-size:11px;">Estoy de acuerdo y acepto que este documento quede bajo propiedad de Plaza Lopez Cotilla S.A de C.V.y/o Sociedad de Información Crediticia consultada para efectos de control y cumplimiento del artículo 28 de la Ley para Regular a Las Sociedades de Información Crediticia; mismo que señala que las Sociedades sólo podrán proporcionar información a un Usuario, cuando éste cuente con la autorización expresa del Cliente mediante su firma autógrafa.</p>
+		<br/>
+		<br/>
+		<br/>
+		<div class="row">
+			<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<hr class="signing">
+		  		<p class="text-center">Nombre Completo Representante Legal</p>
+		 	</div>
+		  	<div class="col-md-6" style="width: 45%; display:inline-block;">
+		  		<hr class="signing">
+		  		<p class="text-center">Firma de Representante Legal</p>
+		  	</div>
+		</div>
+		<br/>
+		<p class="text-center">Para uso exclusivo de la Empresa que efectúa la consulta Plaza Lopez Cotilla S.A de C.V.</p>
+		<br/>
+		<p class="text-left">Fecha de Consulta BC : </p>
+		<br/>
+		<p class="text-left">Folio de Consulta BC : </p>
+		<br/>
+		<p class="text-center"><strong>IMPORTANTE: Este formato debe ser llenado individualmente, para una sola Persona Moral. Area de Nombre y Firma deben ser llenados de puño y letra obligariamente.</strong></p>
+		<br/>
+		<div class="small-text">
+			<p><strong>Aclaraciones</strong></p>
+			<p>1.- La infomacion Proporcionada es Confidencial y asi debe Usted Considerarla</p>
+			<p>2.- El hecho de llenar la presente solicitud no implica ningun compromiso para la administracion para rentarla o no</p>
+			<p>3.- No es obligacion de la admInistracion notificar el motivo por el cual se niegue la aceptacion del presente</p>
+			<p>4.- El compromiso se adquiere hasta el momento en que se termine la investigacion y se haga saber al solicitante la aceptacion</p>
+			<p>5.- La falta de veraciadad de los datos o de cualquiera de los requisitos sera causa de rechazo inmediato</p>
+		</div>
+	</div>
+</form>
